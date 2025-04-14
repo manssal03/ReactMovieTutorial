5999402abf12d3ae38520ce32b245cd5fd76963d
manssal03 <166836285+manssal03@users.noreply.github.com>
2025-04-14 14:44:53 +0200

    MovieApp

---
+++ frontend/src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "ab8e9b46b6bb7638cfa9b15be075a6ea";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+};
